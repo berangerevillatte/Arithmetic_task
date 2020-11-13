9c26f284a921d1451e85e0ffc37d2849b56c537f
Bigras Charlotte <charlotte.bigras@umontreal.ca>
2020-11-13 16:20:05 -0500

    “update_13_nov”

--- mental_arithmetic_task.m
+++ mental_arithmetic_task.m
@@ -1,32 +1,86 @@
-AssertOpenGL
+%code de VILLATTE, Berangere et BIGRAS, Charlotte
 
-screens=Screen('screens'); %repère les écrans
-screenNumber=max(screens); %cherche écran secondaire
-Screen('Preference', 'SkipSyncTests', 1); 
+AssertOpenGL %version compatible de psychtoolbox avec GL
 
-%Pour s'adapter à tous les écrans
-[width_in_mm, height_in_mm]=Screen('DisplaySize', screenNumber);
+screens=Screen('screens'); %repere les ecrans
+screenNumber=max(screens); %cherche ecran secondaire
+Screen('Preference', 'SkipSyncTests', 1);  % put 1 if the sync test fails
+
+%Pour s'adapter a� tous les ecrans
+rect= Screen(screenNumber, 'Rect'); 
 resolution= Screen('Resolution', screenNumber);
-pixel_in_mm = width_in_mm/resolution.width;
-hz=Screen('FrameRate', screenNumber);
+hz=Screen('FrameRate', screenNumber); %creer une frequence dimage
 
-% ouvre l'écran. Mettre la couleur en Red Green Blue [RGB]
-couleur_ecran=[49 140 231];%Ecran bleu 
-[windowPtr, rect]=Screen('OpenWindow',screenNumber, couleur_ecran); 
-%Determiner dimension et position du rectangle
-fromH=resolution.width/4;
-fromV=resolution.height/4;
-toH=fromH*3;
-toV=fromV*3;
+% Get subject name and run number (prompt to get the SubjectName & runNumber before the start of the experiment)
+subjectName = input('Enter participant code: ','s');
+if isempty(subjectName)
+  subjectName = 'trial';
+end
+runNumber = input('Enter the run Number: ','s');
+if isempty(runNumber)
+  runNumber = 'trial';
+end
+
+% Display instructions
+couleur_ecran=[100 100 100];%Ecran gris 
+[w, rect]=Screen('OpenWindow',screenNumber, couleur_ecran); 
 %Dessiner un rectangle coin haut gauche
-Screen('FillRect', windowPtr, [0 255 0], [fromH, fromV, toH, toV]);
-%fonction sur écran secondaire mais pas principal même en changeant l.4 
-%avec screenNumber=min(screens);
-quote{1}='"Vous allez devoir répondre à plusieurs épreuves de calcul mental en 10 secondes maximum"'
-[newX, newY, textHeight]=Screen('DrawText', windowPtr, quote{1}, fromH+10,fromV+10, [0 0 0]);
-Screen('Flip', windowPtr);
+%x1=rect(3)/4;
+%y1=rect(4)/4;
+%x2=x1*3;
+%y2=y1*3;
+%afficher sur ecran bleu
+%Screen('FillRect', w,[255 255 255], [x1, y1, x2, y2]);
 
-%%laisser à la fin
+welcometext = ['Vous aller devoir effectuer une t�che arythm�tique :', char(10), char(10),'Prenez le chiffre 1022 et soustrayez 13, puis soustrayez encore 13, et ainsi de suite.', char(10), char(10),'A chaque fois, vous n''aurez que 7.5 secondes pour repondre.'];
 
-WaitSecs(4);
+%xCenter = round(rect(3)/2);
+%yCenter = round(rect(4)/2);
+
+screen_Center = [rect(3), rect(4)]/2
+DrawFormattedText(w, welcometext,'center','center',[0 0 0]);
+Screen('Flip', w);
+
+%This program waits for the user to press the return key
+[secs, keyCode, deltaSecs] = KbWait([], 2);
+
+welcometext2 = ['Il est donc important que vous soyez rapide et pr�cis, puisque s''il y a une erreur, vous devrez recommencer � 1022.'];
+screen_Center = [rect(3), rect(4)]/2
+DrawFormattedText(w, welcometext2,'center','center',[0 0 0]);
+Screen('Flip', w);
+%[normBoundsRect, offsetBoundsRect]= Screen('TextBounds', w, [welcometext], xCenter, yCenter);
+%offsetX = (offsetBoundsRect(3)-offsetBoundsRect(1))/2;
+%offsetY = (offsetBoundsRect(4)-offsetBoundsRect(2))/2;
+%Screen('DrawText', w, [welcometext], xCenter-offsetX, yCenter-offsetY);
+
+[secs, keyCode, deltaSecs] = KbWait([],2);
+escapeKey=KbName('ESCAPE');
+[a, v, keyCode]=KbCheck;
+if any (keyCode(escapeKey));
+    sca
+end
+%%laisser a la fin
+
+
 sca;
+%% txt logfiles
+if ~exist('logfiles','dir') % ~: not
+    mkdir('logfiles')
+end
+
+EventTxtLogFile = fopen(fullfile('logfiles',[subjectName,'_run_',runNumber,'_Events.txt']),'w');    %fopen: open file
+ fprintf(EventTxtLogFile,'%18s %18s %18s %18s %18s %18s %18s %18s %18s \n',...  %fprintf : write formated dated to txt file
+     'EventNumber','Condition','Coherence','Onset','End','Duration','CorrectResponse','SubjectResponse','isCorrectResponse'); %18s means 18 characters space
+
+ %% %% Save the results
+%save([SubjName,'.mat'])
+save(fullfile('logfiles',[subjectName,'_run_',runNumber,'_all.mat']))   
+
+save(fullfile('logfiles',[subjectName,'_run_',runNumber,'.mat']),...
+'Cfg','conditions','eventCoherence','eventDurations','eventEnds',...
+'eventOnsets','responseTime','eventResponse','correctResponses','isCorrectResponse') 
+ 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ 
