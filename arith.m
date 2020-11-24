ba8821db2590c0896abfa1b2215e5463e5d6f9f3
cbigras <charlotte.bigras@umontreal.ca>
2020-11-24 17:00:03 -0500

    conflicts resolved

--- arith.m
+++ arith.m
@@ -11,21 +11,23 @@ rect= Screen(screenNumber, 'Rect');
 resolution= Screen('Resolution', screenNumber);
 hz=Screen('FrameRate', screenNumber); %creer une frequence dimage
 
+
 %% Get subject name and run number (prompt to get the SubjectName & runNumber before the start of the experiment)
-subjectName = input('Enter participant code: ','s');
-if isempty(subjectName)
-  subjectName = 'trial';
-end
-runNumber = input('Enter the run Number: ','s');
-if isempty(runNumber)
-  runNumber = 'trial';
-end
+% subjectName = input('Enter participant code: ','s');
+% if isempty(subjectName)
+%   subjectName = 'trial';
+% end
+% runNumber = input('Enter the run Number: ','s');
+% if isempty(runNumber)
+%   runNumber = 'trial';
+% end
 
 %% Display a grey screen
 couleur_ecran=[100 100 100];%Ecran gris 
 [w, rect]=Screen('OpenWindow',screenNumber, couleur_ecran); 
-% xCenter = round(rect(3)/2);
-% yCenter = round(rect(4)/2);
+Screen('TextSize', w,[50]);
+xCenter = round(rect(3)/2);
+yCenter = round(rect(4)/2);
 
 %% Escape key
 % This program waits for the user to press the return key
@@ -37,7 +39,7 @@ if any (keyCode(Escape_key));
 end
 
 %% Display instructions
-welcometext = ['Vous aller devoir effectuer une tâche arythmétique :',...
+welcometext = ['Vous allez devoir effectuer une tâche arythmétique :',...
     char(10), char(10),'Prenez le chiffre 1022 et soustrayez 13, puis soustrayez encore 13, et ainsi de suite.', char(10), ...
     char(10),'A chaque fois, vous n''aurez que 7.5 secondes pour repondre.'];
 welcometext2 = ['Il est donc important que vous soyez rapide et précis, puisque s''il y a une erreur, vous devrez recommencer à 1022.'];
@@ -50,6 +52,7 @@ Screen('Flip', w);
 
 DrawFormattedText(w, welcometext2,'center','center',[0 0 0]);
 Screen('Flip', w);
+[secs, keyCode, deltaSecs] = KbWait([], 2);
 
 %% force to exit
 % WaitSecs(5)
@@ -62,49 +65,67 @@ Screen('Flip', w);
 % position = num2str(array(idx));
 % end
 
-%% Calcul mental 
 
+%% Calcul mental 
+pas = 13;
+duration_max = 7.5;
+msg = ['Entrez votre reponse'];
 
 % [string,terminatorChar] = GetEchoString(window,msg,x,y,[textColor],[bgColor],[useKbCheck=0],[deviceIndex],[untilTime=inf],[KbCheck args...]);
 part_resp = str2num(GetEchoString2(w, msg, xCenter, yCenter, [0 0 0],[], 1, []));
-
-pas = 13;
-duration_max = 7.5;
+%function chronometre = chrono(w, duration_max, x, y, color, textsize)
+%chrono(w, duration_max, 300, 400, [255 0 0], 300);
 
 % part_resp = input('1022-13 :');
 msg1 = ['bonne reponse'];
-msg2 = ['mauvaise reponse, veuillez recommencer au debut.'];
-msg3 = ['time out! veuillez recommencer au debut.'];
+msg2 = ['mauvaise reponse, veuillez recommencer du debut.'];
+msg3 = ['Le temps est ecoule! veuillez recommencer du debut.'];
  
 compteur = 1022;
 tic
+start=GetSecs;
+
 while true 
-    
-    b = toc;
-    
-    if part_resp == compteur-13 && (duration_max-b) > 0
+    b = toc;  
+    output = [msg1, part_resp];% enleve msg au debut
+Screen('DrawText', w, output, xCenter, yCenter, [0 0 0],[],[],[]);
+Screen('Flip', w, 0, 1);
+
+    if part_resp == compteur-13 & (duration_max-b) > 0 %bonne reponse
        compteur=compteur-13;
+       
        tic
-        disp(msg1)
-        part_resp = input([num2str(compteur) '-13 :']);      
-        
-     elseif (duration_max-b) == 0 || (duration_max-b) < 0
-         
-        disp(msg3)
-        compteur = 1022;
+      disp(msg1)
+      part_resp = input([num2str(compteur) '-13 :']);      
+
+    elseif (duration_max-b) == 0 || (duration_max-b) < 0 %temps ecoule
+          continue
+   
+          compteur = 1022;
         tic
-        part_resp = input([num2str(compteur) '-13 :']);   
-        
-    elseif isempty(part_resp)
-        part_resp = timeinput(duration_max,'1022');
-        compteur = 1022;
-        tic
-        disp(msg2)
-        part_resp = input([num2str(compteur) '-13 :']);
+          disp(msg3)
+          part_resp = input([num2str(compteur) '-13 :']);  
+   
+      
+%     elseif isempty(part_resp) %mauvaise reponse
+%         part_resp = timeinput(duration_max,'1022');
+%         compteur = 1022;
+%         tic
+%         disp(msg2)
+%         part_resp = input([num2str(compteur) '-13 :']);
+
     else 
         compteur = 1022;
         tic
         disp(msg2)
-        part_resp = input([num2str(compteur) '-13 :']);
-    end
+       part_resp = input([num2str(compteur) '-13 :']);
+
 end
+
+            
+
+
+end
+
+
+
