e2ec5a5645cb9ece81c0830989b5571b9bdf9214
cbigras <charlotte.bigras@umontreal.ca>
2020-11-28 10:59:38 -0500

    28nov

--- arith.m
+++ arith.m
@@ -6,8 +6,7 @@ KbName('UnifyKeyNames');
 screens=Screen('screens'); %repere les ecrans
 screenNumber=max(screens); %cherche ecran secondaire
 Screen('Preference', 'SkipSyncTests', 1);  % put 1 if the sync test fails
-Screen('Preference','TextRenderer',0)
-
+%Screen('Preference','TextRenderer',0) %enlever pour windows
 %% To fit on every screen
 rect= Screen(screenNumber, 'Rect');
 resolution= Screen('Resolution', screenNumber);
@@ -18,7 +17,7 @@ couleur_ecran=[100 100 100];%Ecran gris
 % OS-specific), 1 = HighQ OS-specific (Default), 2 = Linux renderer plugin]);
 
 %% Instructions
-Screen('TextSize', windowPtr,20);
+Screen('TextSize', windowPtr,50);
 xCenter = round(rect(3)/2);
 yCenter = round(rect(4)/2);
 
@@ -51,8 +50,11 @@ duration_max = 7.5;
 
 % DrawFormattedText(windowPtr,Equation_depart,'center', 'center');
 
-msg2 = ['Entrez votre reponse'];
+msg = ['Entrez votre reponse'];
 
+     Screen('TextSize', windowPtr, 50);
+    DrawFormattedText(windowPtr,'1022-13','center', (yCenter-50));
+    Screen('Flip', windowPtr, [], 1);
 % [string,terminatorChar] = GetEchoString(window,msg,x,y,[textColor],[bgColor],[useKbCheck=0],[deviceIndex],[untilTime=inf],[KbCheck args...]);
 part_resp = str2num(GetEchoString2(windowPtr, msg, xCenter, yCenter, [0 0 0],[], 1, []));
 %function chronometre = chrono(w, duration_max, x, y, color, textsize)
@@ -62,47 +64,55 @@ part_resp = str2num(GetEchoString2(windowPtr, msg, xCenter, yCenter, [0 0 0],[],
 message{1} = ['Bonne reponse'];
 message{2} = ['Mauvaise reponse, veuillez recommencer du debut.'];
 message{3} = ['Le temps est ecoule! veuillez recommencer du debut.'];
+message{4} = ['1022-13'];
 
-startSecs = GetSecs;
 compteur = 1022;
-while (StarSecs-GetSecs) < duration_max  
-%     Screen('Flip', w, 0, 1);
-if part_resp == compteur-13 %& (duration_max-b) > 0 %bonne reponse
+%startSecs = GetSecs;
+tic
+
+while true 
+b=toc;
+
+if part_resp == compteur-13 & (duration_max-b) > 0  
    compteur=compteur-13;
-   GetSecs;
+   tic
    pot = 1;
-   part_resp = input([num2str(compteur) '-13 :']);  
    retour = message{pot};
    Screen('TextSize', windowPtr, 50);
    DrawFormattedText(windowPtr,retour,'center', 'center');
    Screen('Flip', windowPtr, [], []);
    WaitSecs(1);
+  % part_resp = input([num2str(compteur) '-13 :']);   
    part_resp = str2num(GetEchoString2(windowPtr, msg, xCenter, yCenter, [0 0 0],[], 1, []));
+   
 
-elseif (duration_max-b) == 0 %|| (duration_max-b) < 0 %temps ecoule
-    compteur = 1022;
-    GetSecs;
+elseif  (duration_max-b) == 0 || (duration_max-b) < 0
     pot = 3;
-    retour = message{pot}
-    part_resp = input([num2str(compteur) '-13 :']); 
+    retour = message{pot};
+    %part_resp = input([num2str(compteur) '-13 :']); 
     Screen('TextSize', windowPtr, 50);
     DrawFormattedText(windowPtr,retour,'center', 'center');
     Screen('Flip', windowPtr, [], []);
     WaitSecs(1);
+    compteur = 1022;
+    tic
     part_resp = str2num(GetEchoString2(windowPtr, msg, xCenter, yCenter, [0 0 0],[], 1, []));
 
-elseif any (keyCode(escapeKey));
-    sca
 else 
     compteur = 1022;
-    GetSecs;
+    tic
     pot = 2;
-    retour = message{pot}
-    part_resp = input([num2str(compteur) '-13 :']);
+    retour = message{pot};
+   % part_resp = input([num2str(compteur) '-13 :']);
     Screen('TextSize', windowPtr, 50);
     DrawFormattedText(windowPtr,retour,'center', 'center');
     Screen('Flip', windowPtr, [], []);
     WaitSecs(1);
+    pot = 4;
+    retour = message{pot};
+     Screen('TextSize', windowPtr, 50);
+    DrawFormattedText(windowPtr,retour,'center', (yCenter-50));
+    Screen('Flip', windowPtr, [], 1);
     part_resp = str2num(GetEchoString2(windowPtr, msg, xCenter, yCenter, [0 0 0],[], 1, []));
     
     end
