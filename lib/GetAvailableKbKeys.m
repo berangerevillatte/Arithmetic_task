b5e8f6c1003e2d6b3d1b7571b9cc54fdaa49e5e5
taumont <tomy.aumont@umontreal.ca>
2022-03-31 19:19:01 -0400

    Fix the output generation when many outputs are requested

--- lib/GetAvailableKbKeys.m
+++ lib/GetAvailableKbKeys.m
@@ -9,10 +9,8 @@ cancelKey = KbName('c');
 
 % Assign output in requested format
 if nargout==1
-    varargout = {struct('next', spaceKey, ...
-                        'quit', escapeKey, ...
-                        'cancel', cancelKey)};
-elseif nargout==5
+    varargout = {struct('next', spaceKey, 'quit', escapeKey, 'cancel', cancelKey)};
+elseif nargout==3
     varargout = {spaceKey, escapeKey, cancelKey};
 end
 
